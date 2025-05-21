ead16c699576bb7e9840445e3ddc1da9f65335fd
eeduca <lemongrab97@gmail.com>
2025-05-21 21:23:58 +0200

    Functions drawScreenMenu and drawAsideItems

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,16 +1,16 @@
 ﻿const products = [
-	{
-		id: 1,
-		name: 'Coca Cola',
-		price: 1.55,
+    {
+        id: 1,
+        name: 'Coca Cola',
+        price: 1.55,
         image: 'picsum.photos/256/256'
-	},
-	{
-		id: 2,
-		name: 'Kapucino',
-		price: 2.60,
-		image: 'picsum.photos/256/256'
-	},
+    },
+    {
+        id: 2,
+        name: 'Kapucino',
+        price: 2.60,
+        image: 'picsum.photos/256/256'
+    },
     {
         id: 3,
         name: 'Espresso',
@@ -63,77 +63,50 @@
 
 const screenTable = document.getElementById('screen-table');
 const screenMenu = document.getElementById('screen-menu');
+const screenAside = document.getElementById('screen-aside');
 const tables = screenTable.querySelectorAll('.btn-table');
 const btnBack = document.getElementById('btn-back');
 
 tables.forEach((table) => {
-	table.addEventListener('click', (event) => {
-		const tableId = Number(event.target.getAttribute('data-table-id') ?? -1);
-		if (tableId === -1) {
-			return;
-		}
+    table.addEventListener('click', (event) => {
+        const tableId = Number(event.target.getAttribute('data-table-id') ?? -1);
+        if (tableId === -1) {
+            return;
+        }
 
-		if (tableId === 0) {
-			console.log('Izabrana je kasa')
-		}
-		else {
-			console.log(`Izabran je sto ${tableId}`)
-		}
+        if (tableId === 0) {
+            console.log('Izabrana je kasa')
+        }
+        else {
+            console.log(tableId);
+            drawScreenMenu();
+            drawAsideItems(tableId);
+        }
 
-		screenTable.classList.add('hidden');
-		screenMenu.classList.remove('hidden');
-		btnBack.classList.remove('hidden');
+        screenTable.classList.add('hidden');
+        screenMenu.classList.remove('hidden');
+        btnBack.classList.remove('hidden');
 
+        //console.log('Table clicked:', event.target.getAttribute('data-table-id') ?? -1);
 
-		//console.log('Table clicked:', event.target.getAttribute('data-table-id') ?? -1);
-
-		return;
-	});
+        return;
+    });
 });
+
+function drawScreenMenu() {
+    products.forEach((product) => {
+        let newElement = document.createElement('div');
+        newElement.textContent = product.name;
+        newElement.classList.add('testClass');
+        screenMenu.appendChild(newElement);
+    });
+}
+function drawAsideItems(id) {
+    console.log(id);   
+}
 
 btnBack.addEventListener('click', (event) => {
-	screenTable.classList.remove('hidden');
-	screenMenu.classList.add('hidden');
+    screenTable.classList.remove('hidden');
+    screenMenu.classList.add('hidden');
     btnBack.classList.add('hidden');
 });
-function changeContent(page) {
-	const containerDiv = document.getElementById('container'); 
-
-	switch (page) {
-		case 'home':
-			{
-				containerDiv.innerHTML = `
-				
-				<h2>
-					Welcome to the Home Page!
-				</h2>
-				<p>
-					This is the main page.
-				</p>
-				<p>
-					Explore the different sections using
-					the navigation menu.
-				</p>
-			`;
-				break;
-			}
-		default:
-			{
-				containerDiv.innerHTML = `
-				
-				<h2>
-					Error Page Not Found!
-				</h2>
-				<p>
-					Page was not found.
-				</p>
-				<p>
-					Explore the different sections using
-					the navigation menu.
-				</p>
-			`;
-				break;
-			}
-
-	}
-	}
