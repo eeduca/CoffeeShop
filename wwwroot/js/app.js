56da82665925bc6ee764ac538cbefe4502263762
eeduca <lemongrab97@gmail.com>
2025-05-23 22:31:03 +0200

    Made changes in app.js. Removed setAtribute method

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -61,9 +61,30 @@
     }
 ]
 
+const orderList = [
+    {
+        quantity: 4,
+        price: 4.80,
+        productName: "Caj"
+    },
+    {
+        quantity: 1,
+        price: 1.55,
+        productName: "Coca-Cola"
+    },
+    {
+        quantity: 2,
+        price: 3.60,
+        productName: "Sok"
+    }
+
+]
+
 const screenTable = document.getElementById('screen-table');
 const screenMenu = document.getElementById('screen-menu');
-const screenAside = document.getElementById('screen-aside');
+const screenAside = document.querySelector('aside');
+const asideItems = document.getElementById('order-list');
+const asideBottom = screenAside.querySelector(".aside-bottom")
 const tables = screenTable.querySelectorAll('.btn-table');
 const btnBack = document.getElementById('btn-back');
 
@@ -73,19 +94,20 @@ tables.forEach((table) => {
         if (tableId === -1) {
             return;
         }
-
-        if (tableId === 0) {
-            console.log('Izabrana je kasa')
-        }
         else {
+            asideItems.innerHTML = ''; // Clear previous items
+            screenMenu.innerHTML = ''; // Clear previous items
             console.log(tableId);
             drawScreenMenu();
+            screenMenu.scrollTop = 0; // Reset scroll position
             drawAsideItems(tableId);
         }
 
         screenTable.classList.add('hidden');
         screenMenu.classList.remove('hidden');
         btnBack.classList.remove('hidden');
+        asideBottom.classList.remove('hidden');
+        asideItems.classList.remove('hidden');
 
         //console.log('Table clicked:', event.target.getAttribute('data-table-id') ?? -1);
 
@@ -94,19 +116,81 @@ tables.forEach((table) => {
 });
 
 function drawScreenMenu() {
+
     products.forEach((product) => {
-        let newElement = document.createElement('div');
-        newElement.textContent = product.name;
-        newElement.classList.add('testClass');
-        screenMenu.appendChild(newElement);
+        let productCard = document.createElement('div');
+        productCard.classList.add('product-box');
+        screenMenu.appendChild(productCard);
+
+        let productName = document.createElement('div');
+        productName.textContent = product.name;
+        productCard.appendChild(productName);
+
+        let productImg = document.createElement('img');
+        productImg.src = `https://${product.image}`;
+        productImg.alt = product.name;
+        productImg.width = 256;
+        productImg.height = 256; 
+        productImg.style.width = "100%";
+        productImg.style.height = "auto";
+        productImg.style.display = "block";
+        productCard.appendChild(productImg);
+
+        let itemPrice = document.createElement('div');
+        itemPrice.textContent = `${product.price} e`;
+        itemPrice.style.display = "inline";
+        productCard.appendChild(itemPrice);
+
+        let btnAdd = document.createElement('button');
+        btnAdd.textContent = '+';
+        productCard.appendChild(btnAdd);
     });
 }
 function drawAsideItems(id) {
-    console.log(id);   
+
+
+    let title = document.createElement('div');
+    title.textContent = "Order List";
+    title.textAlign = "center";
+    title.style.marginBottom ="1em";
+    asideItems.appendChild(title);
+
+    orderList.forEach((orderItem) => {
+        let orderItemBox = document.createElement('div');
+        orderItemBox.style.padding= "1em 1em 1em 0";
+        asideItems.appendChild(orderItemBox);
+
+        let itemName = document.createElement('span');
+        itemName.textContent = orderItem.productName;
+        itemName.style.display = "inline-block";
+        itemName.style.width = "33%";
+        itemName.style.textAlign= "left";
+        orderItemBox.appendChild(itemName);
+
+        let itemQuantity = document.createElement('span');
+        itemQuantity.textContent = orderItem.quantity;
+        itemName.style.display = "inline-block";
+        itemName.style.width = "33%";
+        itemName.style.textAlign = "center";
+        orderItemBox.appendChild(itemQuantity);
+
+        let itemPrice = document.createElement('span');
+        itemPrice.textContent = `${orderItem.price} e`;
+        itemName.style.display = "inline-block";
+        itemName.style.width = "33%";
+        itemName.style.textAlign = "right";
+        orderItemBox.appendChild(itemPrice);
+
+    });
+
+    //screenAsideItem
+    //console.log(id);   
 }
 
 btnBack.addEventListener('click', (event) => {
     screenTable.classList.remove('hidden');
     screenMenu.classList.add('hidden');
     btnBack.classList.add('hidden');
+    asideBottom.classList.add('hidden');
+    asideItems.classList.add('hidden');
 });
